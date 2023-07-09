6ba8acbda897872a345315d6f4b1f907650e94d6
Peerayukun <peerayuphengsuwan@gmail.com>
2023-07-09 23:24:14 +0700

    frontend api register

--- react_chat/src/components/Register.js
+++ react_chat/src/components/Register.js
@@ -12,35 +12,64 @@ const Register = () => {
     const [password, setPassword] = useState('')
     const [confirm, setConfirm] = useState('')
     const [matchPassword, setMatchPassword] = useState(true)
+    const [usernameError, setUsernameError] = useState(false)
+    const [emailError, setEmailError] = useState(false)
+    const [passwordError, setPasswordError] = useState(false)
     function handlePassword(confirmPassword){
         setMatchPassword(confirmPassword === password)
         setConfirm(confirmPassword)
     }
     async function submitRegisterPayload(){
-        const payload = {
+        const body = {
             'username':username,
             'email':email,
             'firstname':firstname,
             'lastname':lastname,
             'password':password
         }
-        const registerResponse = await axios.post(`${API_BASE_URL}/auth/register`,payload)
-        if(registerResponse.status === 200){
-            console.log('ok')
+        try{
+            await axios.post(`${API_BASE_URL}/auth/register`,body)
+            window.location.replace('/login')
         }
-        else{
-            console.log('not ok')
+        catch(error){
+            const errorInputs = error.response.data.error
+            if(errorInputs.includes('username') || errorInputs.includes('duplicate username')){
+                if (errorInputs.includes('username')){
+                    alert('invalid format of username (must be alphanumeric)')
+                }
+                else{
+                    alert('duplicate username')
+                }
+                setUsernameError(true)
+            }
+            if(errorInputs.includes('email') || errorInputs.includes('duplicate email')){
+                if (errorInputs.includes('email')){
+                    alert('invalid format of email')
+                }
+                else{
+                    alert('duplicate email')
+                }
+                setEmailError(true)
+            }
+            if(errorInputs.includes('password')){
+                alert('password is empty')
+                setPasswordError(true)
+            }
         }
     }
     return (
         <div className="mainFrame">
         <a href='/'><img src={homeLogo} className='homeLogo' alt=''></img></a>
             <h1 className="titleChat">Register</h1>
-            <input placeholder='username' onChange={(event)=>{setUsername(event.target.value)}}></input>
-            <input placeholder='email' onChange={(event)=>{setEmail(event.target.value)}}></input>
+            <input placeholder='username' 
+            onChange={(event)=>{setUsername(event.target.value);setUsernameError(false);}}
+            className={usernameError?'errorInput':''}></input>
+            <input placeholder='email' onChange={(event)=>{setEmail(event.target.value);setEmailError(false);}}
+            className={emailError?'errorInput':''}></input>
             <input placeholder='firstname' onChange={(event)=>{setFirstname(event.target.value)}}></input>
             <input placeholder='lastname' onChange={(event)=>{setLastname(event.target.value)}}></input>
-            <input placeholder='password' type='password' onChange={(event)=>{setPassword(event.target.value);setConfirm('');}}></input>
+            <input placeholder='password' type='password' onChange={(event)=>{setPassword(event.target.value);setConfirm('');setPasswordError(false);}}
+            className={passwordError?'errorInput':''}></input>
             <input placeholder='confirm password' 
             className={matchPassword?'':'errorInput'} 
             type='password' 
