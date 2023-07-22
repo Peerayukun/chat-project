638b07375b1fe367c533a3a1f7d07a417bfc906e
Peerayukun <peerayuphengsuwan@gmail.com>
2023-07-22 13:29:11 +0700

    update api match frontend requirement

--- django_chat/chat/views.py
+++ django_chat/chat/views.py
@@ -5,7 +5,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import SessionAuthentication
 import json
 from .models import *
-from ..django_chat.config import DB_NAME
+from django_chat.config import DB_NAME
 
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
@@ -66,7 +66,7 @@ def leave_room(request):
 @permission_classes([IsAuthenticated])
 @authentication_classes([SessionAuthentication])
 def rooms(request):
-    result = [(i.id, i.name, i.type) for i in ChatRoom.objects.raw("""select id, name, case when room_id is null then 'public' else 'joined' end as type from {0}.chat_chatroom t1 left join 
+    result = [{"id":i.id,"name": i.name,"type": i.type} for i in ChatRoom.objects.raw("""select id, name, case when room_id is null then 'public' else 'joined' end as type from {0}.chat_chatroom t1 left join 
                                                                 (select distinct(room_id) from {1}.chat_chatroommember where user_id={2} and status='joined') t2 
                                                                 on t1.id = t2.room_id""".format(DB_NAME,DB_NAME,request.user.id))
     ]
@@ -78,7 +78,7 @@ def rooms(request):
 def room_messages(request,room_id):
     member = ChatRoomMember.objects.filter(room__id=room_id,user=request.user).first()
     if member:
-        message_objs = ChatMessage.objects.filter(send_from__room__id=room_id)
+        message_objs = ChatMessage.objects.filter(send_from__room__id=room_id).order_by('created')
         result = []
         for message_obj in message_objs:
             if message_obj.send_from.status == "joined":
@@ -87,12 +87,23 @@ def room_messages(request,room_id):
                 sender_name = "left member"
             result.append(
                 {"sender_name":sender_name, 
+                 "self": message_obj.send_from.user == request.user,
                 "message":message_obj.message, 
-                "send_time":message_obj.created.timestamp()}
+                "send_time":message_obj.created.timestamp()*1000}
             )
         return HttpResponse(json.dumps(result))
     else:
         return HttpResponse('not a member',status=400)
+    
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+@authentication_classes([SessionAuthentication])
+def room_info(request,room_id):
+    room = ChatRoom.objects.filter(id=room_id).first()
+    if room:
+        return HttpResponse(json.dumps({"id":room.id,"name":room.name}))
+    else:
+        return HttpResponse('no room',status=404)
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
