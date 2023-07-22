b3516a06fa5119eb5db8ab884dc784f9b69ccad6
Peerayukun <peerayuphengsuwan@gmail.com>
2023-07-22 13:19:07 +0700

    render init api result

--- react_chat/src/components/chat_components/Rooms.js
+++ react_chat/src/components/chat_components/Rooms.js
@@ -1,6 +1,37 @@
 import './Rooms.css'
 import { Card } from './Card'
+import { useContext, useEffect, useState } from 'react'
+import { CsrftokenContext } from '../Landing'
+import { API_BASE_URL } from '../../config'
+import axios from 'axios'
+
 export const Rooms =()=>{
+    const getCsrftoken = useContext(CsrftokenContext)
+    const [joinedRoom,setJoinedRoom] = useState([])
+    const [publicRoom,setPublicRoom] = useState([])
+    useEffect(()=>{
+        async function callRooms(){
+            let joinedRoomRes = []
+            let publicRoomRes = []
+            try{
+            const roomRes = await axios.get(`${API_BASE_URL}/chat/rooms`,{withCredentials:true})
+            roomRes.data.forEach(element => {
+                if(element.type === 'joined'){
+                    joinedRoomRes.push({roomId:element.id, roomName:element.name})
+                }
+                else if(element.type === 'public'){
+                    publicRoomRes.push({roomId:element.id, roomName:element.name})
+                }
+            });
+            }
+            catch(error){
+                console.log(error)
+            }
+            setJoinedRoom(joinedRoomRes)
+            setPublicRoom(publicRoomRes)
+        }
+        callRooms()
+    },[])
     return (
     <div className='roomsFrame'>
         <div className='roomsContainer'>
@@ -10,11 +41,11 @@ export const Rooms =()=>{
             </div>
             <h3>Joined</h3>
             <div id="joined-room" className='cardContainer'>
-                <Card/>
+                {joinedRoom.map(element=><Card room={element} key={element.roomId}/>)}
             </div>
             <h3>Public</h3>
             <div id="public-room" className='cardContainer'>
-                <Card/>
+                {publicRoom.map(element=><Card room={element} key={element.roomId}/>)}
             </div>
         </div>
     </div>)
--- react_chat/src/components/chat_components/Sender.js
+++ react_chat/src/components/chat_components/Sender.js
@@ -1,16 +1,18 @@
 import defaultProfilePic from "../../assets/images/profile.png"
 
-export const Sennder =()=>{
-    const now = new Date()
+export const Sennder =({props})=>{
+    const sendTime = new Date(props.send_time)
     return(
         <div className='sender'>
+        <div className='senderFlex'>
             <div className='messagerPic' style={{marginLeft:'10px'}}>
                 <img src={defaultProfilePic} alt=''  style={{widows:'35px', height:'35px'}}></img>
             </div>
             <div className="messageBubble" style={{backgroundColor:'#b0e0e6', marginLeft:'5px'}}>
-                <p style={{fontSize:'12px', textAlign: 'center', color: 'black'}}>your message</p>
+                <p style={{fontSize:'12px', textAlign: 'center', color: 'black'}}>{props.message}</p>
             </div>
-            <p className="messageSendTime">{now.toDateString()}<br/>{now.toLocaleTimeString()}</p>
-        </div>   
+            <p className="seidTextMessage">{sendTime.toDateString()}<br/>{sendTime.toLocaleTimeString()}</p>
+        </div>  
+        </div> 
     )
 }
