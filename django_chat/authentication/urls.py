5ac432ff0674821dd444510a614a8868111e1565
Peerayukun <peerayuphengsuwan@gmail.com>
2023-07-23 13:34:48 +0700

    api update profile

--- django_chat/authentication/urls.py
+++ django_chat/authentication/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('register', views.register, name='register'),
     path('user', views.user, name='user'),
-    path('logout', views.logout, name='logout')
-
+    path('logout', views.logout, name='logout'),
+    path('update_profile',views.update_profile,name="update_profile")
 ]
